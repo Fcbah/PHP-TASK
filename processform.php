69f145324a74d39e151a11972bd7531cad8e2295
Akinleye Hephzibah B <fcbah1248@gmail.com>
2020-03-26 18:12:58 +0100

    The form has been successfully completed

--- processform.php
+++ processform.php
@@ -1,9 +1,24 @@
 <?php
 $fName = $_GET["first_name"];
 $lName = $_GET["last_name"];
-$Name = $fName."".$lName.".txt";
-$myfile = fopen($Name, 'w');
-fwrite($myfile,implode(" \n",$_GET));
+$Name = $fName."_".$lName.".txt";
+$myfile = fopen($Name,'a');
+fwrite($myfile,"\n\n".implode(" \n",$_GET));
 fclose($myfile);
-print_r($_GET);
+echo('
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Record Registered</title>
+</head>
+<body>
+    <h3>Contact Form Registration Success</h3>
+    <p>'.$fName.', your Contact Records and Message Has been successfully Registered in the file <b>'.$Name.'</b></p>
+    <p>Click to return to <a href="index.html">Contact Form</a> </p>
+</body>
+</html>
+');
 ?>
